0411e9a8cef7e8053b41aa496f10966062ecc477
ekamleen <ekamleenm@gmail.com>
2024-04-29 14:45:41 -0700

    Profile on going

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from db import Database
 
 app = Flask(__name__)
@@ -25,7 +25,23 @@ def perform_regis():
     if response == 1:
         return render_template('login.html', message='Registration successful! Now Login ⤵')
     else:
-        return  render_template('register.html', message='Email already exists try again ⤵')
+        return render_template('register.html', message='Email already exists try again ⤵')
+
+
+@app.route("/perform_login", methods=['post'])
+def perform_login():
+    email = request.form.get('user_email')
+    password = request.form.get('user_password')
+    response = dbo.search(email, password)
+    if response == 1:
+        return redirect('/profile')
+    else:
+        return render_template('login.html', message='Incorrect Credentials, Try Again ⤵')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', message='Welcome')
 
 
 app.run(debug=True)
